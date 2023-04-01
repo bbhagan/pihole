c51188e20aba68813228ee8f31d5c038f8920d9d
Brian Hagan <bbhagan@gmail.com>
2023-04-01 16:54:17 -0700

    First impl of install script

--- install_docker_pihole.sh
+++ install_docker_pihole.sh
@@ -1,9 +1,22 @@
 echo 'apt-get update'
 sudo apt-get update
+
 echo 'apt-get upgrade'
 sudo apt-get upgrade -y
-echo 'install docker'
-curl -fsSL https://get.docker.com -o get-docker.sh | bash
-echo 'give docker permissions'
-sudo usermod -aG docker $USER
-curl -fssl https://github.com/
+
+#echo 'install docker'
+#curl -fsSL https://get.docker.com -o get-docker.sh | bash
+
+#echo 'give docker permissions'
+#sudo usermod -aG docker $USER
+
+echo 'cloning repo'
+git clone https://github.com/bbhagan/pihole
+
+cd pihole
+echo 'running docker containers'
+sudo docker compose up -d
+
+echo ' '
+echo 'pihole password'
+sudo docker logs pihole 2>&1 | grep random
