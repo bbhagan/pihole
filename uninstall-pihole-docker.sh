1b71778f3c01e9bc8117e13058e956de576af6ae
Brian Hagan <bbhagan@gmail.com>
2023-04-01 21:24:31 -0700

    Fixing uninstall

--- uninstall-pihole-docker.sh
+++ uninstall-pihole-docker.sh
@@ -4,6 +4,7 @@ sudo systemctl stop pihole-docker-compose
 sudo systemctl disable pihole-docker-compose
 sudo systemctl status pihole-docker-compose
 
+sudo rm /etc/systemd/system/pihole-docker-compose.service
 sudo rm -rf /srv/docker/pihole
 rm ./pihole_install.log
 rm ./uninstall-pihole-docker.sh
