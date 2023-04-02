9e1010ab312474fbecee997cc2342f221771943e
Brian Hagan <bbhagan@gmail.com>
2023-04-01 18:35:04 -0700

    Add test

--- install.sh
+++ install.sh
@@ -27,8 +27,14 @@ echo 'Running docker containers'
 echo ' '
 sudo docker compose up -d
 
+#sleep is to make sure everything is totally up before testing
+sleep 3
+
 echo ' '
-echo 'Sleep 5'
-sleep 5
-echo 'Get password'
+echo 'Test services'
+echo 'Google IP via cloudflared: $(dig @127.0.0.1 -p 5053 +short google.com)'
+echo 'Google IP via pihole: $(dig @127.0.0.1 +short google.com)'
+
+echo ' '
+echo 'PASSWORD:'
 echo "$(sudo docker logs pihole 2>&1 | grep random)"
