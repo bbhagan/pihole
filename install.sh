df422e61545a90e20747250214491188b7e63260
Brian Hagan <bbhagan@gmail.com>
2023-04-01 21:48:20 -0700

    Cleanup of loging path

--- install.sh
+++ install.sh
@@ -1,41 +1,52 @@
 #! /bin/bash
 
-echo ' '
-echo 'Running apt-get update'
-sudo apt-get update &>> ./pihole_install.log
+PATH=$(pwd)
+INSTALL_LOG=$PATH/pihole_install.log
 
-echo ' '
+echo " "
+echo 'Running apt-get update'
+sudo apt-get update &>> $INSTALL_LOG
+
+echo " "
 echo 'Running apt-get upgrade'
-sudo apt-get upgrade -y &>> ./pihole_install.log
+sudo apt-get upgrade -y &>> $INSTALL_LOG
 
 #echo 'install docker'
 #curl -fsSL https://get.docker.com -o get-docker.sh | bash
 
-#echo 'give docker permissions'
-#sudo usermod -aG docker $USER
-
 echo ' '
 echo 'Cloning repo'
-sudo git clone https://github.com/bbhagan/pihole /srv/docker/pihole &>> ./pihole_install.log
+sudo git clone https://github.com/bbhagan/pihole /srv/docker/pihole &>> $INSTALL_LOG
 sudo cp /srv/docker/pihole/pihole-docker-compose.service /etc/systemd/system/pihole-docker-compose.service
 cp /srv/docker/pihole/uninstall-pihole-docker.sh .
 
 echo ' '
-echo 'Running docker containers'
+echo 'Booting docker containers'
+cd srv/docker/pihole
+sudo docker compose up -d
+test_services;
+sudo docker compose down
 
-sudo systemctl enable pihole-docker-compose &>> ./pihole_install.log
-sudo systemctl start pihole-docker-compose &>> ./pihole_install.log
-sudo systemctl status pihole-docker-compose &>> ./pihole_install.log
+echo " "
+echo "Making a service and starting"
+sudo systemctl enable pihole-docker-compose &>> $INSTALL_LOG
+sudo systemctl start pihole-docker-compose &>> $INSTALL_LOG
+sudo systemctl status pihole-docker-compose &>> $INSTALL_LOG
 
 #sleep is to make sure everything is totally up before testing
 sleep 5
 
-echo ' '
-echo 'Test services'
-echo "Google IP via cloudflared: $(dig @127.0.0.1 -p 5053 +short google.com)"
-echo "Google IP via pihole: $(dig @127.0.0.1 +short google.com)"
+test_services;
 
 echo " "
 echo "PASSWORD:"
 echo "$(sudo docker logs pihole 2>&1 | grep random)"
+
+test_services() {
+echo " "
+echo "Test services"
+echo "Google IP via cloudflared: $(dig @127.0.0.1 -p 5053 +short google.com)"
+echo "Google IP via pihole: $(dig @127.0.0.1 +short google.com)"
+}
+
 
