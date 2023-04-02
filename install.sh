88ccfeffe1d5a0ee0e21b82ca0e0f9f667e43e5c
Brian Hagan <bbhagan@gmail.com>
2023-04-01 21:56:25 -0700

    Substitute var

--- install.sh
+++ install.sh
@@ -7,18 +7,18 @@ echo "INSTALL_LOG: $INSTALL_LOG"
 
 echo " "
 echo "Running apt-get update"
-sudo apt-get update &>> $INSTALL_LOG
+sudo apt-get update &>> /home/pi/pihole_install.log
 
 echo " "
 echo "Running apt-get upgrade"
-sudo apt-get upgrade -y &>> $INSTALL_LOG
+sudo apt-get upgrade -y &>> /home/pi/pihole_install.log
 
 #echo 'install docker'
 #curl -fsSL https://get.docker.com -o get-docker.sh | bash
 
 echo ' '
 echo 'Cloning repo'
-sudo git clone https://github.com/bbhagan/pihole /srv/docker/pihole &>> $INSTALL_LOG
+sudo git clone https://github.com/bbhagan/pihole /srv/docker/pihole &>> /home/pi/pihole_install.log
 sudo cp /srv/docker/pihole/pihole-docker-compose.service /etc/systemd/system/pihole-docker-compose.service
 cp /srv/docker/pihole/uninstall-pihole-docker.sh .
 
@@ -31,9 +31,9 @@ sudo docker compose down
 
 echo " "
 echo "Making a service and starting"
-sudo systemctl enable pihole-docker-compose &>> $INSTALL_LOG
-sudo systemctl start pihole-docker-compose &>> $INSTALL_LOG
-sudo systemctl status pihole-docker-compose &>> $INSTALL_LOG
+sudo systemctl enable pihole-docker-compose &>> /home/pi/pihole_install.log
+sudo systemctl start pihole-docker-compose &>> /home/pi/pihole_install.log
+sudo systemctl status pihole-docker-compose &>> /home/pi/pihole_install.log
 
 #sleep is to make sure everything is totally up before testing
 sleep 5
