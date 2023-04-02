60b8910974a77491bd17940698d913b3501de48d
Brian Hagan <bbhagan@gmail.com>
2023-04-01 20:22:03 -0700

    Moving stuff around

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 
+echo 'Cloning repo'
+git clone https://github.com/bbhagan/pihole &> ./pihole_install.log
+
 echo 'Running apt-get update'
-sudo apt-get update &> /dev/null
+sudo apt-get update &> ./pihole_install.log
 
 echo ' '
 echo 'Running apt-get upgrade'
-sudo apt-get upgrade -y &> /dev/null
+sudo apt-get upgrade -y &> ./pihole_install.log
 
 #echo 'install docker'
 #curl -fsSL https://get.docker.com -o get-docker.sh | bash
@@ -13,15 +16,9 @@ sudo apt-get upgrade -y &> /dev/null
 #echo 'give docker permissions'
 #sudo usermod -aG docker $USER
 
-echo ' '
-echo 'Cloning repo'
-echo ' '
-git clone https://github.com/bbhagan/pihole
-
 cd pihole
 echo ' '
 echo 'Running docker containers'
-echo ' '
 sudo docker compose up -d
 
 #sleep is to make sure everything is totally up before testing
@@ -30,8 +27,10 @@ sleep 3
 echo ' '
 echo 'Test services'
 echo "Google IP via cloudflared: $(dig @127.0.0.1 -p 5053 +short google.com)"
-echo "$(dig @127.0.0.1 +short google.com)"
+echo "Google IP via pihole: $(dig @127.0.0.1 +short google.com)"
 
 echo " "
 echo "PASSWORD:"
 echo "$(sudo docker logs pihole 2>&1 | grep random)"
+
+cp ../pihole_install.log .
