e1b686e694e1a5deb18f8e4650a763b8d764c454
Brian Hagan <bbhagan@gmail.com>
2023-04-01 22:00:24 -0700

    D'oh... PATH

--- install.sh
+++ install.sh
@@ -1,24 +1,24 @@
 #! /bin/bash
 
-PATH=$(pwd)
-echo "PATH: $PATH"
-INSTALL_LOG=$PATH/pihole_install.log
+INSTALL_INIT_PATH=$(pwd)
+echo "INSTALL_PATH: $INSTALL_INIT_PATH"
+INSTALL_LOG=$INSTALL_INIT_PATH/pihole_install.log
 echo "INSTALL_LOG: $INSTALL_LOG"
 
 echo " "
 echo "Running apt-get update"
-sudo apt-get update &>> /home/pi/pihole_install.log
+sudo apt-get update &>> $INSTALL_LOG
 
 echo " "
 echo "Running apt-get upgrade"
-sudo apt-get upgrade -y &>> /home/pi/pihole_install.log
+sudo apt-get upgrade -y &>> $INSTALL_LOG
 
 #echo 'install docker'
 #curl -fsSL https://get.docker.com -o get-docker.sh | bash
 
 echo ' '
 echo 'Cloning repo'
-sudo git clone https://github.com/bbhagan/pihole /srv/docker/pihole &>> /home/pi/pihole_install.log
+sudo git clone https://github.com/bbhagan/pihole /srv/docker/pihole &>> $INSTALL_LOG
 sudo cp /srv/docker/pihole/pihole-docker-compose.service /etc/systemd/system/pihole-docker-compose.service
 cp /srv/docker/pihole/uninstall-pihole-docker.sh .
 
@@ -31,9 +31,9 @@ sudo docker compose down
 
 echo " "
 echo "Making a service and starting"
-sudo systemctl enable pihole-docker-compose &>> /home/pi/pihole_install.log
-sudo systemctl start pihole-docker-compose &>> /home/pi/pihole_install.log
-sudo systemctl status pihole-docker-compose &>> /home/pi/pihole_install.log
+sudo systemctl enable pihole-docker-compose &>> $INSTALL_LOG
+sudo systemctl start pihole-docker-compose &>> $INSTALL_LOG
+sudo systemctl status pihole-docker-compose &>> $INSTALL_LOG
 
 #sleep is to make sure everything is totally up before testing
 sleep 5
