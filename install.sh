e14c0aa6fd1a6c13a1f66825fbdb273f60535532
Brian Hagan <bbhagan@gmail.com>
2023-04-01 18:16:36 -0700

    Trying to pipe to file

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
 
+echo ' '
 echo 'apt-get update'
+echo ' '
 sudo apt-get update
 
+echo ' '
 echo 'apt-get upgrade'
+echo ' '
 sudo apt-get upgrade -y
 
 #echo 'install docker'
@@ -12,12 +16,16 @@ sudo apt-get upgrade -y
 #echo 'give docker permissions'
 #sudo usermod -aG docker $USER
 
+echo ' '
 echo 'cloning repo'
+echo ' '
 git clone https://github.com/bbhagan/pihole
 
 cd pihole
+echo ' '
 echo 'running docker containers'
+echo ' '
 sudo docker compose up -d
 
 echo ' '
-echo "$(sudo docker logs pihole 2>&1 | grep random)"
+sudo docker logs pihole 2>&1 | grep random >> pihole_password.txt
